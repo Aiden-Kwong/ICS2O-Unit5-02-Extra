b5d42431488655cf00402e2f14720e464ef7096b
AidenKwong <>
2021-06-01 15:17:32 +0000

    comment fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//This detects any clicks on the button.//
+// This detects any clicks on the button, if there are any it triggers the function called change.
 document.getElementById('button').addEventListener('click', change)
 
-//This function will change, in order, the font size, font type and font color of any paragraph with the id 'para'.//
+// This function will change, in order, the font size, font type and font color of any paragraph with the id 'para'.
 function change () {
   document.getElementById('para').style.fontSize = 20
   document.getElementById('para').style.fontFamily = 'sans-serif'
