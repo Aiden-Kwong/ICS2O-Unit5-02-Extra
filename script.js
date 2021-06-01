345904e8fa21945d79f7010b2d468adf87f89549
AidenKwong <>
2021-06-01 15:12:12 +0000

    Comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//This detects any clicks on the button.//
 document.getElementById('button').addEventListener('click', change)
 
+//This function will change, in order, the font size, font type and font color of any paragraph with the id 'para'.//
 function change () {
-  document.getElementById('para').style.fontSize = 20;
+  document.getElementById('para').style.fontSize = 20
   document.getElementById('para').style.fontFamily = 'sans-serif'
   document.getElementById('para').style.color = '#3e5c76'
 }
