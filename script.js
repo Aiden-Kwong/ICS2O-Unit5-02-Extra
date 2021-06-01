1ae8f97104e30a675955c8dff7720d0a7296f80a
AidenKwong <>
2021-06-01 15:07:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click', change)
+
+function change () {
+  document.getElementById('para').style.fontSize = 20;
+  document.getElementById('para').style.fontFamily = 'sans-serif'
+  document.getElementById('para').style.color = '#3e5c76'
+}
